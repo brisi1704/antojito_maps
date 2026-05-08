39bc179bd4ced07fd7c9d52c7caa54a41f65daa4
Douglas Mendez/Jala University <douglas.mendez@jala.university>
2026-05-07 20:05:07 -0400

    fix: add firebaseConfig to environment.production.ts to resolve missing Firebase options in prod build

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://antojitos-maps-6fafc945e691.herokuapp.com'
+  apiBaseUrl: 'https://antojitos-maps-6fafc945e691.herokuapp.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyBAyNpkUTQhCOF7j7qbl3dCxb1DSapxbVc",
+    authDomain: "antojitos-maps-auth.firebaseapp.com",
+    projectId: "antojitos-maps-auth",
+    storageBucket: "antojitos-maps-auth.firebasestorage.app",
+    messagingSenderId: "487116595412",
+    appId: "1:487116595412:web:ef35ac38bc5644841adb41",
+    measurementId: "G-T67RM18CKC"
+  }
 };
